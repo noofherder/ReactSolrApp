c4e5e14b19b02c3bf327394e89bc72ec4aa44c1d
Tom Mortimer <tom@husky.lan>
2016-05-19 09:24:38 +0100

    made hashHistory explicit

--- source/app.js
+++ source/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect, Link } from 'react-router';
+import { Router, Route, Redirect, Link, hashHistory } from 'react-router';
 import NavBar from './components/navbar';
 import SearchApp from './components/searchapp';
 
@@ -17,7 +17,7 @@ const Contact = () =>
   <h1>Contact</h1>;
 
 const RoutedApp = () =>
-  <Router>
+  <Router history={hashHistory}>
     <Redirect from="/" to="/search" />
     <Route path="/" component={App}>
       <Route path="search" component={SearchApp} />
